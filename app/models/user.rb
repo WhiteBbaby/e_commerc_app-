6467f1329ffbbb4168b81aa913c224bafcf069e1
WhiteBbaby <124869286+WhiteBbaby@users.noreply.github.com>
2023-11-24 13:24:22 +0300

    General page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, length: { maximum: 150 }
+  validates :first_name, length: { maximum: 100 }
+  validates :last_name, length: { maximum: 100 }
 end
